8694117afdef142f3964d4e21cffad8fd895b86c
Propromp <p@propromp.net>
2021-07-18 17:00:34 +0900

    fix example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -6,30 +6,6 @@ dependencies {
     implementation(project(":api"))
 }
 tasks {
-    processResources {
-        this.duplicatesStrategy=DuplicatesStrategy.INHERIT
-        filesMatching("*.yml") {
-            expand(project.properties)
-        }
-
-        from(sourceSets.main.get().resources.srcDirs) {
-            filter {
-                it.replace("@version@", "1.16.5-R0.1-SNAPSHOT")
-            }
-        }
-    }
-    shadowJar {
-        archiveFileName.set("pcf-example-v$version.jar")
-    }
-    create<Copy>("buildToPluginsDirectory") {
-        File("server/plugins").listFiles()?.forEach {
-            if (it.name.contains("pcf-example-v") && it.isFile) {
-                it.delete()
-            }
-        }
-        from(shadowJar)
-        into("server/plugins")
-    }
     compileJava {
         options.compilerArgs.add("-parameters")
     }

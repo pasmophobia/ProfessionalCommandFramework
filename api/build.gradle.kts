9ac87793ffa4a0cbef9ba42e2afd7233a8a3cf42
Propromp <p@propromp.net>
2021-07-18 16:30:38 +0900

    fix

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,10 +1,19 @@
 group = "net.propromp"
 version = "2.0"
-
+plugins {
+    `maven-publish`
+}
 dependencies {
     compileOnly("com.mojang:brigadier:1.0.17")
 }
-
+publishing {
+    publications {
+        register<MavenPublication>("jitpack"){
+            artifactId="pcf"
+            from(components["java"])
+        }
+    }
+}
 tasks {
     compileJava {
         sourceCompatibility = "1.8"

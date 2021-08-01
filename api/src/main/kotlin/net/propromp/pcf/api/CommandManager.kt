308f675ff36f6660f1d30bae95b9704b547a7828
Propromp <p@propromp.net>
2021-08-01 12:23:41 +0900

    3.1.1

--- api/src/main/kotlin/net/propromp/pcf/api/CommandManager.kt
+++ api/src/main/kotlin/net/propromp/pcf/api/CommandManager.kt
@@ -2,7 +2,6 @@ package net.propromp.pcf.api
 
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
-import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import com.mojang.brigadier.tree.CommandNode
 import net.propromp.pcf.nms.NMS
 import org.bukkit.Bukkit
@@ -12,20 +11,20 @@ import org.bukkit.plugin.Plugin
 /**
  * Professional command manager
  *
- * @property dispatcher command dispatcher
+ * @property brigadierDispatcher command dispatcher
  * @constructor Create empty Command manager
  */
 class CommandManager(val plugin: Plugin){
-    val dispatcher:CommandDispatcher<Any>
-    val bukkitDispatcher:Any
+    private val brigadierDispatcher:CommandDispatcher<Any>
+    private val minecraftDispatcher:Any
     init {
         val console = NMS.fromClass(Bukkit.getServer().javaClass).getField(Bukkit.getServer(),"console")!!
-        bukkitDispatcher = NMS("MinecraftServer").invokeMethod(console,"getCommandDispatcher")!!
-        dispatcher = NMS.fromClass(bukkitDispatcher.javaClass).getField(bukkitDispatcher,"b") as CommandDispatcher<Any>
+        minecraftDispatcher = NMS("MinecraftServer").invokeMethod(console,"getCommandDispatcher")!!
+        brigadierDispatcher = NMS.fromClass(minecraftDispatcher.javaClass).getField(minecraftDispatcher,"b") as CommandDispatcher<Any>
     }
     fun register(command:PcfCommand){
-        val node = dispatcher.register(command.getLiteralArgumentBuilder())
-        val bukkitCommand = NMS.getCraftBukkit("command.VanillaCommandWrapper").getConstructor(CommandDispatcher::class.java,CommandNode::class.java).newInstance(dispatcher,node) as BukkitCommand
+        val node = brigadierDispatcher.register(command.getLiteralArgumentBuilder())
+        val bukkitCommand = NMS.getCraftBukkit("command.VanillaCommandWrapper").getConstructor(NMS.getNMS("CommandDispatcher"),CommandNode::class.java).newInstance(minecraftDispatcher,node) as BukkitCommand
         bukkitCommand.permission=null
         Bukkit.getCommandMap().register(plugin.name,bukkitCommand)
         LiteralArgumentBuilder.literal<Any>("a")
